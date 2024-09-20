592679357f2344b0986db89091f66a7f26aa7dad
adamsafril19 <adamsafrila0@gmail.com>
2024-09-21 01:02:19 +0700

    Prak 2.2 – Not Found Exceptions - JS4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class UserController extends Controller
 {
@@ -45,9 +46,15 @@ class UserController extends Controller
         // $user = UserModel::firstwhere('level_id', 1); // ambil model yang pertama dari batasan yang ditentukan
         // return view('user', ['data' => $user]);
 
-        $user = UserModel::findOr(20,['username', 'nama'], function(){
-            abort(404);
-        });
+        // $user = UserModel::findOr(20,['username', 'nama'], function(){
+        //     abort(404);
+        // });
+        // return view('user', ['data' => $user]);
+
+        // $user = UserModel::findOrFail(1);
+        // return view('user', ['data' => $user]);
+
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
     }
     
