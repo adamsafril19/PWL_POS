cfbfc3741c007a5e11b8563f2a32ebf11341f4d7
adamsafril19 <adamsafrila0@gmail.com>
2024-09-24 19:53:19 +0700

    Prak 2.7 - Relations - JS4

---
+++ app/Models/LevelModel.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class LevelModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'm_level';
+
+    /**
+     * Mendefinisikan relasi bahwa level ini terkait dengan satu pengguna.
+     *
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(UserModel::class);
+    }
+}
